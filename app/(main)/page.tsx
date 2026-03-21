2cdeef406f0def87fc858835a3491a23a562873f
juniordev203 <ttk11092k3@gmail.com>
2026-03-21 17:21:44 +0700

    update title

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -50,7 +50,7 @@ export default function HomePage() {
             </div>
 
             {/* Floating reaction bar */}
-            <ReactionBar />
+            {/* <ReactionBar /> */}
         </div>
     )
 }
